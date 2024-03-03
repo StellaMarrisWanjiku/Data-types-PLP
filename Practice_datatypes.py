f760a9badfc8666da3e77f68e79387a55bf518ff
wanjikustl19 <wanjikumarris@gmail.com>
2024-03-03 16:46:37 +0300

    added a data types practice of week 2 plp

---
+++ Practice_datatypes.py
@@ -0,0 +1,40 @@
+a = 100
+b= 3.14
+#print(a*b)
+c=5+7j
+#print(a+b-c)
+d=False
+#print(d)
+
+str = "Welcome to PLP Academy"
+#print(str)
+#print(str[0])
+#print(str[-1])
+#print(str[4:10])
+plp = "Python Class"
+#print(str+plp)
+#print(plp * 4)
+
+Shopping_list = ["bread", "milk", 20,40.5, 5+8]
+#print(Shopping_list)
+Building_List = [450, "wheelbarrows", 180, "shovels", "bread", "milk"]
+#print(Building_List)
+#print(Shopping_list[0])
+#print(Shopping_list[4:5])
+#print(Shopping_list + Building_List)
+#print(Shopping_list * 2)
+plp_tuple = "abcd", 786, 2.23, "john", 70.2
+#print(plp_tuple)
+tiny_tuple = ('John', 5+7j)
+#print(tiny_tuple)
+#print(plp_tuple + tiny_tuple)
+
+dict = {}
+dict['one'] =  "this is one"
+dict[2] =  "this is two"
+tinydict = {'name':'John' , 'code':6769, 'dept':1769}
+print(tinydict.keys())
+print(dict['one'])
+print(dict[2])
+print(tinydict.values())
+print(tinydict)
